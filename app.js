89733cdc7a592ceaf879d0bd0960689885cdcb4b
Matt Schroder <m3schroder@gmail.com>
2021-03-17 06:56:33 +0000

    Added .env

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var con = require('./config/db.js');
+require('dotenv').config();
 
+console.log(process.env.DB_HOST);
 var sql = "INSERT INTO user (username, email, password) VALUES ('mataroni', 'm3schroder', 'guessthisisit')";
 con.query( sql, function (error, results, fields) {
   if (error) throw error;
