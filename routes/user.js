4166eac448bf914d75235cdf8005ae52a1a49501
Matt Schroder <m3schroder@gmail.com>
2021-05-06 13:09:24 -0500

    provides endpoints for external application

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,26 @@
-const express = require('express')
-const router = express.Router()
-const userModel = require("../models/user_model")
-const wrap = require('../middleware/wrap')
+const express = require("express");
+const router = express.Router();
+const userModel = require("../models/user_model");
+const ash = require("express-async-handler");
 
+router.get(
+  "/",
+  ash(async (req, res, next) => {
+    await userModel.getUsers(function (result) {
+      res.json(result);
+    });
+  })
+);
 
-router.get('/', wrap(async (req, res, next) => {
-	await userModel.getUsers(function(result){
-		res.json(result)
-	})
-}))
+router.post(
+  "/",
+  ash(async (req, res, next) => {
+    let { username, email, password } = req.body;
+    await userModel.postUser(username, email, password, function (result) {
+      console.log(result);
+      res.send("Its good I guess");
+    });
+  })
+);
 
-router.post('/', wrap(async (req, res, next) => {
-	let {username, email, password} = req.body
-	await userModel.postUser(username, email, password, function(result){
-		console.log(result)
-		res.send("Its good I guess")
-	})
-}))
-
-module.exports = router
+module.exports = router;
