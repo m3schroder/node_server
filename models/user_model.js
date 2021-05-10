6c8ec44cc1e8c5c6cd43f70a09276f9b0762341d
Matt Schroder <m3schroder@gmail.com>
2021-05-10 14:15:52 -0500

    implementation of basic crud operations

--- models/user_model.js
+++ models/user_model.js
@@ -12,7 +12,7 @@ const postUser = (username, email, password, callback) => {
       }
     );
   } catch (e) {
-    console.log("There was an error posting new user: " + e);
+    console.log("There was an error posting new user: ?", e);
   }
 };
 
@@ -24,8 +24,44 @@ const getUsers = (callback) => {
       return callback(results);
     });
   } catch (e) {
-    console.log("There was an error getting user data: " + e);
+    console.log("There was an error getting user data:\n ?", e);
   }
 };
 
-module.exports = { postUser, getUsers };
+const findUser = (username, callback) => {
+  try {
+    var sql = "SELECT * FROM app_db.user WHERE username = ?";
+    connection.query(sql, username, function (e, result, fields) {
+      if (e) throw e;
+      return callback(result);
+    });
+  } catch (e) {
+    console.log("There was an error finding ?:\n ?", username, e);
+  }
+};
+
+const deleteUser = (username, callback) => {
+  try {
+    var sql = "DELETE FROM app_db.user WHERE username = ?";
+    connection.query(sql, username, function (e, result, fields) {
+      if (e) throw e;
+      return callback(result);
+    });
+  } catch (e) {
+    console.log("There was an error finding ?:\n ?", username, e);
+  }
+};
+
+const updateUser = (username, email, callback) => {
+  try {
+    var sql = "UPDATE user SET email = ?  WHERE username = ?";
+    connection.query(sql, [email, username], function (e, result, fields) {
+      if (e) throw e;
+      return callback(result);
+    });
+  } catch (e) {
+    console.log("There was an error finding ?:\n ?", username, e);
+  }
+};
+
+module.exports = { postUser, getUsers, findUser, deleteUser, updateUser };
